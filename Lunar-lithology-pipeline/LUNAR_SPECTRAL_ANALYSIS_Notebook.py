13d641f9cd82ce0f83cbb48eae249ab211adeb31
Fermy <123935594+KarnakOza@users.noreply.github.com>
2026-02-15 10:30:05 +0530

    Update LUNAR_SPECTRAL_ANALYSIS_Notebook.py

--- Lunar-lithology-pipeline/LUNAR_SPECTRAL_ANALYSIS_Notebook.py
+++ Lunar-lithology-pipeline/LUNAR_SPECTRAL_ANALYSIS_Notebook.py
@@ -1,6 +1,6 @@
 # Correct RDN file paths
 hdr = HDR_FILE_PATH
-img = HDR_FILE_PATH
+img = IMG_FILE_PATH
 
 # Load image
 m3_data = io.envi.open(hdr, image=img)
@@ -10,12 +10,9 @@ cube = m3_data.load()
 print("✅ Loaded cube shape:", cube.shape)  # Expecting (1182, 304, 85)
 
 
-
 transposed_cube = np.transpose(cube, (1, 2, 0))
-
 #Assuming 'cube' is your data with shape (1182, 304, 3)
 #Transpose the data to shape (304, 3, 1182)
-
 band_index = 45
 if band_index < transposed_cube.shape[2]:
     band_image = transposed_cube[:, :, band_index]
@@ -46,5 +43,26 @@ bdi_map = compute_band_depth(cube, center_band=45, left_band=40, right_band=50)
 
 plt.imshow(bdi_map, cmap='viridis')
 plt.title("Band Depth Index (950 nm)")
+plt.axis('off')
+plt.show()
+
+
+###FOR LUNAR SLOPE MAP
+# Paths to the HDR and IMG
+hdr_path = HDR_FILE_PATH
+img_path = IMG_FILE_PATH
+# Load the file
+obs_cube = io.envi.open(hdr_path, img_path)
+
+print("Shape:", obs_cube.shape)
+print("Band Names:", obs_cube.metadata.get('band names', 'Not found'))
+
+# Example: Display the Slope band (Band 7 or 8 depending on 0-index)
+slope_band = obs_cube.read_band(7)  # Band 8 is usually slope
+
+plt.figure(figsize=(6, 6))
+plt.imshow(slope_band, cmap='terrain')
+plt.colorbar(label='Slope (degrees?)')
+plt.title("Lunar Slope Map (from OBS)")
 plt.axis('off')
 plt.show()
